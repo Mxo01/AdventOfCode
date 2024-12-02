6754c64944d83225c92248c020b7c61ff4b10ff5
Mxo01 <mariodimodica.01@gmail.com>
2024-12-02 22:06:11 +0100

    Day 2 completed

---
+++ 2024/Day 2 - Red-Nosed Reports/1.1.py
@@ -0,0 +1,29 @@
+def main():
+    safe_reports_count = 0
+
+    f = open("puzzle.txt", "r")
+
+    for line in f:
+        line = list(map(int, line.split(" ")))
+
+        if is_line_safe(line):
+            safe_reports_count += 1
+
+    print(safe_reports_count)
+
+
+def is_line_safe(line):
+    return is_sorted(line) and are_all_levels_safe(line)
+
+
+def is_sorted(line):
+    return sorted(line) == line or sorted(line, reverse=True) == line
+
+def are_all_levels_safe(line):
+    return all(
+        1 <= abs(int(line[i]) - int(line[i + 1])) <= 3 for i in range(len(line) - 1)
+    )
+
+
+if __name__ == "__main__":
+    main()
