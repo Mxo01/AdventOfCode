49c70694fa6a5c50b0727387d4c3dd5e0b8238bb
Mario Di Modica <mario.dimodica@nttdata.com>
2024-12-06 12:34:13 +0100

    2024 Day 6

---
+++ 2024/Day 6 - Guard Gallivant/2.py
@@ -0,0 +1,57 @@
+def main():
+    lines = read_puzzle()
+
+    x, y, direction = find_guard(lines)
+    starting_x, starting_y, starting_direction = x, y, direction
+    possible_obstacles = 0
+
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            if lines[i][j] == "#" or lines[i][j] in ["^", ">", "v", "<"]:
+                continue
+
+            visited = set()
+            x, y, direction = starting_x, starting_y, starting_direction
+
+            while True:
+                if (x, y, direction) in visited:
+                    possible_obstacles += 1
+                    break
+                else:
+                    visited.add((x, y, direction))
+
+                if should_stop(lines, x, y, direction):
+                    break
+
+                next_x, next_y = update_position(x, y, direction)
+                next = lines[next_x][next_y]
+
+                if next == "#" or next_x == i and next_y == j:
+                    direction = update_direction(direction)
+                    continue
+
+                x, y = next_x, next_y
+
+    print(possible_obstacles)
+
+def find_guard(lines):
+    return next((i, line.index(symbol), direction) for i, line in enumerate(lines) for symbol, direction in {"^": "top", ">": "right", "v": "bottom", "<": "left"}.items() if symbol in line)
+
+def should_stop(lines, x, y, direction):
+    return x == 0 and direction == "top" or x == len(lines) - 1 and direction == "bottom" or y == 0 and direction == "left" or y == len(lines[x]) - 1 and direction == "right"
+
+def update_direction(direction):
+    return {"top": "right", "right": "bottom", "bottom": "left", "left": "top"}[direction]
+
+def update_position(x, y, direction):
+    return {"top": (x - 1, y), "right": (x, y + 1), "bottom": (x + 1, y), "left": (x, y - 1)}[direction]
+
+def read_puzzle():
+    f = open("puzzle.txt", "r")
+    lines = f.read().splitlines()
+    f.close()
+
+    return lines
+
+if __name__ == '__main__':
+	main()
